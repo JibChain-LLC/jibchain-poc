254064f61fb02790079c1a9c52067243cc1dfd71
Drin Sadiku <Drin@telos-labs.com>
2024-11-27 16:06:23 +0100

    refactor

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,83 @@
 "use Client"
 import React from 'react';
-import Dashboard from "./_components/dashboard-page"
+import { Card } from '#/components/ui/card';
+import { dashboardCardData } from '#/utils/utils';
+import Image from 'next/image';
+import { Button } from '#/components/ui/button';
+import Jumbotron from '../../../images/jumbotron.jpg';
+import RiskContainer from './_components/risk-container';
+import { createClient } from '#/lib/supabase/server';
+import { redirect } from 'next/navigation';
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const supabase = await createClient();
+  const {
+    error,
+    data: { user }
+  } = await supabase.auth.getUser();
+
+  if (error || !user) redirect('/logout');
+  const { firstName, lastName } = user.user_metadata;
+
   return (
-    <div className='w-full'>
-      <Dashboard/>
-    </div>
+      <div className="grid grid-cols-1 lg:grid-cols-7 pt-4 bg-gray-50">
+        <div className="lg:col-span-5 flex flex-col space-y-6 ">
+          <Card className="border-none shadow-none bg-gray-50">
+            <h1 className="text-2xl lg:text-3xl font-semibold">Welcome, {firstName } {lastName }</h1>
+          </Card>
+          <div className='flex flex-col items-start gap-3'>
+          <h3 className="text-lg lg:text-xl font-medium text-gray-700">
+              Featured News
+            </h3>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {dashboardCardData.map((card, index) => (
+              <Card
+                key={index}
+                className="bg-white  rounded-xl overflow-hidden"
+              >
+                <Image
+                  src={card.image}
+                  alt="Feature"
+                  className="w-full h-32 lg:h-48 object-cover"
+                />
+                <div className="p-4 flex flex-col">
+                  <p className="text-gray-700 text-base weight lg:text-sm mb-4 font-medium">
+                    {card.description}
+                  </p>
+                  <Button className="w-28 bg-white text-green-800 border border-green-800 hover:bg-green-600 hover:text-white font-semibold transition duration-300">
+                    {card.buttonText}
+                  </Button>
+                </div>
+              </Card>
+            ))}
+          </div>
+          </div>
+          <Card className="relative w-full min-h-[300px] lg:min-h-[390px] rounded-xl overflow-hidden">
+            <Image
+              src={Jumbotron}
+              alt="Operational Resilience"
+              className="absolute w-full h-full object-cover"
+            />
+            <div className="absolute inset-0 bg-black opacity-70"></div>
+            <div className="relative flex items-center justify-center text-white px-4 py-6 h-full">
+              <div className="text-center">
+                <h1 className="text-lg sm:text-xl md:text-2xl lg:text-5xl font-bold">
+                  Operational Resilience with AI
+                </h1>
+                <p className="text-sm sm:text-base md:text-lg mt-2 sm:mt-4 max-w-4xl mx-auto">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae fugit possimus sunt nobis doloremque consequuntur!
+                </p>
+                <Button className="bg-green-600 hover:bg-green-700 mt-4 lg:mt-6 px-4 py-2 lg:px-6 lg:py-3">
+                  Learn More
+                </Button>
+              </div>
+            </div>
+          </Card>
+        </div>
+        <div className="lg:col-span-2 bg-gray-50 h-full lg:p-4 lg:pl-10">
+          <RiskContainer />
+        </div>
+      </div>
+    
   );
 }
