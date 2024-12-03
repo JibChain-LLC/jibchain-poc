da9b83fa7524e0d68ba3e2f59e990420f8b549b3
Drin Sadiku <Drin@telos-labs.com>
2024-12-03 14:20:49 +0100

    fixed overview card with the new images

--- src/components/overview/overview-card-component.tsx
+++ src/components/overview/overview-card-component.tsx
@@ -8,26 +8,24 @@ const OverviewCardComponent = () => {
     <div className='grid grid-cols-1 gap-6 rounded-none p-5 lg:grid-cols-2 xl:grid-cols-3'>
       {overviewCardData.map((item, i) => (
         <Card
-          className='flex h-[132px] items-center justify-center rounded-md border-none bg-gray-100 shadow-md transition-all hover:shadow-lg'
+          className='rounded-md border border-transparent bg-transparent shadow-md hover:shadow-lg hover:border-green-400 transition-all duration-500 hover:text-green-700 '
           key={i}>
-          <CardHeader className='w-full border-none bg-transparent p-0'>
-            <div className='flex flex-row items-center'>
-              <div className='w-full'>
-                <CardTitle className='p-5 text-xl font-medium text-black'>
-                  <span className='flex flex-col'>
-                    <span className='text-slate-700'>{item.title}</span>
-                    <span className='text-[32px] font-bold'>{item.value}</span>
-                  </span>
-                </CardTitle>
-              </div>
-              <div className='flex w-full items-center justify-end'>
-                <Image
-                  src={item.icon}
-                  alt={item.alt}
-                  className='w-full object-cover xl:w-auto'
-                />
-              </div>
-            </div>
+          <CardHeader className='border-none bg-transparent p-0 flex-row justify-between flex w-full items-center  rounded-lg  bg-gray-100 hover:bg-green-50'>
+                  <CardTitle className='p-5 text-xl font-medium  w-1/2'>
+                    <span className='flex flex-col'>
+                      <span className=''>{item.title}</span>
+                      <span className='text-[32px] font-bold '>
+                        {item.value}
+                      </span>
+                    </span>
+                  </CardTitle>
+                  <div className='flex justify-end items-center'>
+                  <Image
+                    src={item.icon}
+                    alt={item.alt}
+                    className='h-[150px] object-cover opacity-40 w-full'
+                  />
+                  </div>
           </CardHeader>
         </Card>
       ))}
