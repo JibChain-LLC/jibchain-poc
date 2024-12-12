468a1725fc41dca58fff6840acb9057d85ebeb0d
jbukuts <jbukuts@gmail.com>
2024-12-12 13:54:41 -0500

    fix: separate maps per page, fixed colors, added tooltip on bullet hover

--- src/components/global-impact/country-risk-probability.tsx
+++ src/components/global-impact/country-risk-probability.tsx
@@ -3,30 +3,48 @@
 import * as am5 from '@amcharts/amcharts5';
 import * as am5map from '@amcharts/amcharts5/map';
 import am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
+import am5themes_Responsive from '@amcharts/amcharts5/themes/Responsive';
 import am5geodata_worldLow from '@amcharts/amcharts5-geodata/worldLow';
+/* @ts-expect-error no available typings */
+import { geoCylindricalStereographic } from 'd3-geo-projection';
+import { Building } from 'lucide-react';
 import { useLayoutEffect } from 'react';
+import ReactDOMServer from 'react-dom/server';
 import { cn } from '#/lib/utils';
-import {
-  bubble_data,
-  BubbleData,
-  CountryDataContext,
-  risk_data
-} from '#/utils/utils';
-const getFillColor = (dataContext: CountryDataContext | undefined) => {
-  const country = risk_data.find((data) => data.id === dataContext?.id);
-  if (country) {
-    switch (country.risk) {
-      case 'High':
-        return am5.color(0xff0000); // Red for High Risk
-      case 'Moderate':
-        return am5.color(0xffa500); // Orange for Moderate Risk
-      case 'Low':
-        return am5.color(0x00ff00); // Green for Low Risk
-      default:
-        return am5.color(0xcccccc); // Default gray
-    }
-  }
-  return am5.color(0xcccccc); // Default for countries without data
+import { bubble_data, BubbleData } from '#/utils/utils';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+
+const BulletTooltip = () => {
+  return (
+    <div className='flex flex-col gap-4 p-1.5'>
+      <div className='flex flex-col gap-1'>
+        <Avatar className='size-10'>
+          <AvatarImage src='' />
+          <AvatarFallback>
+            <Building size={16} />
+          </AvatarFallback>
+        </Avatar>
+        <p className='text-sm font-semibold text-white'>Dynamic Capital</p>
+        <p className='text-sm font-thin text-gray-400'>South America +2</p>
+      </div>
+
+      <div className='flex flex-col gap-1'>
+        <p className='text-sm font-normal leading-none text-gray-200'>
+          Accounting software and services
+        </p>
+        <div className='flex gap-2.5 text-sm font-semibold text-white'>
+          <div className='flex'>
+            <p>Exposure: </p>
+            <p>Low</p>
+          </div>
+          <div className='flex'>
+            <p>Impact: </p>
+            <p>Medium</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const createPointSeries = (chart: am5map.MapChart, root: am5.Root) => {
@@ -43,116 +61,85 @@ const createPointSeries = (chart: am5map.MapChart, root: am5.Root) => {
   pointSeries.bullets.push((root, series, dataItem) => {
     const context = dataItem.dataContext as BubbleData;
 
+    const tooltip = am5.Tooltip.new(root, {
+      pointerOrientation: 'horizontal',
+      getFillFromSprite: false,
+      autoTextColor: false,
+      animationDuration: 0,
+      stateAnimationDuration: 0,
+      showTooltipOn: 'hover',
+      keepTargetHover: true,
+      background: am5.PointedRectangle.new(root, { cornerRadius: 6 })
+    });
+
+    tooltip.get('background')?.setAll({
+      fill: am5.color(0x111928),
+      fillOpacity: 1
+    });
+
     const mainCircle = am5.Circle.new(root, {
       radius: 8,
       fill: context.color,
-      strokeWidth: 2
-    });
-
-    const animatedBorder = am5.Circle.new(root, {
-      radius: 11,
       stroke: am5.Color.lighten(context.color, 0.6),
-      strokeWidth: 8
+      strokeWidth: 2,
+      tooltip: tooltip
     });
 
-    animatedBorder.animate({
-      key: 'strokeOpacity',
-      from: 0.4,
-      to: 0.9,
-      duration: 1300,
+    mainCircle.set(
+      'tooltipHTML',
+      ReactDOMServer.renderToString(<BulletTooltip />)
+    );
+    mainCircle.animate({
+      key: 'strokeWidth',
+      from: 2,
+      to: 8,
+      duration: 3500,
       loops: Infinity,
-      easing: am5.ease.linear
+      easing: am5.ease.yoyo(am5.ease.linear)
     });
 
-    const container = am5.Container.new(root, {
-      centerX: am5.p50,
-      centerY: am5.p50
-    });
-
-    container.children.push(mainCircle);
-    container.children.push(animatedBorder);
-
-    return am5.Bullet.new(root, { sprite: container });
+    return am5.Bullet.new(root, { sprite: mainCircle });
   });
 };
-const CountryRiskProbability = ({ supplier }: { supplier: boolean }) => {
+
+const CountryRiskProbability = () => {
   useLayoutEffect(() => {
     const root = am5.Root.new('chartdiv');
-    root.setThemes([am5themes_Animated.new(root)]);
+    root.setThemes([
+      am5themes_Responsive.new(root),
+      am5themes_Animated.new(root)
+    ]);
 
     const chart = root.container.children.push(
       am5map.MapChart.new(root, {
-        projection: am5map.geoMercator(),
+        projection: geoCylindricalStereographic().parallel(0),
         panX: 'none',
         panY: 'none',
-        wheelY: 'zoom',
-        maxZoomLevel: supplier ? 10 : 2
+        wheelY: 'none'
       })
     );
 
     if (root._logo) root._logo.dispose();
 
-    const polygonSeries = chart.series.push(
+    chart.series.push(
       am5map.MapPolygonSeries.new(root, {
+        fill: am5.color(0xf3f4f6),
+        stroke: am5.color(0x9ca3af),
         geoJSON: am5geodata_worldLow,
         exclude: ['AQ']
       })
     );
 
-    polygonSeries.mapPolygons.template.set('fill', am5.color(0xcccccc));
-    polygonSeries.mapPolygons.template.set('tooltipText', '{name}: {risk}');
-
-    if (supplier) {
-      polygonSeries.mapPolygons.template.adapters.add('fill', (fill, target) =>
-        getFillColor(target.dataItem?.dataContext as CountryDataContext)
-      );
-    } else {
-      createPointSeries(chart, root);
-    }
-
-    const zoomInButton = document.getElementById('zoomIn');
-    const zoomOutButton = document.getElementById('zoomOut');
-
-    const handleZoomIn = () => chart.zoomIn();
-    const handleZoomOut = () => chart.zoomOut();
-
-    if (zoomInButton) zoomInButton.addEventListener('click', handleZoomIn);
-    if (zoomOutButton) zoomOutButton.addEventListener('click', handleZoomOut);
+    createPointSeries(chart, root);
 
     return () => {
       root.dispose();
-      if (zoomInButton) zoomInButton.removeEventListener('click', handleZoomIn);
-      if (zoomOutButton)
-        zoomOutButton.removeEventListener('click', handleZoomOut);
     };
-  }, [supplier]);
+  }, []);
 
   return (
     <div className='relative mt-1'>
-      <div
-        id='chartdiv'
-        className={cn(
-          'w-full',
-          supplier ? 'h-[160px] max-w-[700px]' : 'h-[600px] max-w-[1200px]'
-        )}></div>
-      <div className='absolute right-2 top-2'>
-        <button
-          id='zoomIn'
-          className={cn(
-            'mx-1 rounded-full border px-2',
-            'border-gray-300 hover:bg-gray-200'
-          )}>
-          +
-        </button>
-        <button
-          id='zoomOut'
-          className={cn(
-            'rounded-full border px-2',
-            'border-gray-300 hover:bg-gray-200'
-          )}>
-          -
-        </button>
-      </div>
+      <div id='chartdiv' className={cn('h-auto min-h-[450px] w-full')}></div>
     </div>
   );
 };
--- src/components/global-impact/global-impact-table.tsx
+++ src/components/global-impact/global-impact-table.tsx
@@ -1,14 +1,5 @@
-import { type ColumnDef, Table as TableType } from '@tanstack/react-table';
-import { ChevronDown } from 'lucide-react';
+import { type ColumnDef } from '@tanstack/react-table';
 import { Badge } from '#/components/ui/badge';
-import { Button } from '#/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuTrigger
-} from '#/components/ui/dropdown-menu';
-import { Input } from '#/components/ui/input';
 import { supplierTable } from '#/utils/utils';
 import { DataTable } from '../ui/data-table';
 
@@ -18,22 +9,13 @@ type Supplier = {
   exposureToRisk: 'Low' | 'Medium' | 'High';
   impactOperation: 'Low' | 'Medium' | 'High';
 };
-interface ControlsProps {
-  table: TableType<Supplier>;
-}
+
 const columns: ColumnDef<Supplier>[] = [
   {
     accessorKey: 'supplier',
     header: 'Supplier',
     cell: ({ row }) => (
       <div className='capitalize'>{row.getValue('supplier')}</div>
-    )
-  },
-  {
-    accessorKey: 'region',
-    header: 'Region',
-    cell: ({ row }) => (
-      <div className='capitalize'>{row.getValue('region')}</div>
     )
   },
   {
@@ -55,75 +37,29 @@ const columns: ColumnDef<Supplier>[] = [
     }
   },
   {
+    accessorKey: 'region',
+    header: 'Region',
+    cell: ({ row }) => (
+      <div className='capitalize'>{row.getValue('region')}</div>
+    )
+  },
+  {
     accessorKey: 'impactOperation',
-    header: 'Impact to Operation',
-    cell: ({ row }) => {
-      const impactOperation = row.getValue<string>('impactOperation');
-      const dotColor =
-        impactOperation === 'High'
-          ? 'bg-red-500'
-          : impactOperation === 'Medium'
-            ? 'bg-yellow-500'
-            : 'bg-green-500';
-
-      return (
-        <div className='flex items-center'>
-          <span className={`mr-2 size-2 rounded-full ${dotColor}`}></span>
-          {impactOperation}
-        </div>
-      );
-    }
+    header: 'Impact to Operation'
   }
 ];
 
 export function GlobalImpactTable() {
-  const Controls = ({ table }: ControlsProps) => (
-    <div className='flex items-center p-4'>
-      <Input
-        placeholder='Filter suppliers...'
-        value={(table.getColumn('supplier')?.getFilterValue() as string) ?? ''}
-        onChange={(event) =>
-          table.getColumn('supplier')?.setFilterValue(event.target.value)
-        }
-        className='max-w-sm bg-transparent text-black'
-      />
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button
-            variant='outline'
-            className='ml-auto bg-transparent text-black'>
-            Columns <ChevronDown className='ml-2 size-4' />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align='end'>
-          {table
-            .getAllColumns()
-            .filter((column) => column.getCanHide())
-            .map((column) => (
-              <DropdownMenuCheckboxItem
-                key={column.id}
-                className='capitalize'
-                checked={column.getIsVisible()}
-                onCheckedChange={(value) => column.toggleVisibility(!!value)}>
-                {column.id}
-              </DropdownMenuCheckboxItem>
-            ))}
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
-  );
-
   return (
     <DataTable
       columns={columns}
       data={supplierTable}
-      controls={Controls}
       pagination={{
         manual: false,
         pageSize: 10
       }}
       tableClassName='bg-white'
-      wrapperClassName='mx-4'
+      wrapperClassName='mt-6'
     />
   );
 }
